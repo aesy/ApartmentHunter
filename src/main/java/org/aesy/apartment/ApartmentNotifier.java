1734dcbd6e66cb18cd2386d9e3b3972978f2285e
Isak W <isak.wertwein@gmail.com>
2018-04-20 00:10:42 +0200

    Fix event listener ordering

--- src/main/java/org/aesy/apartment/ApartmentNotifier.java
+++ src/main/java/org/aesy/apartment/ApartmentNotifier.java
@@ -4,6 +4,7 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -31,6 +32,7 @@ public class ApartmentNotifier {
         this.templateEngine = templateEngine;
     }
 
+    @Order(0)
     @EventListener
     public void notifyNewApartments(NewApartmentsEvent event) {
         List<Apartment> apartments = event.getApartments();
--- src/main/java/org/aesy/apartment/ApartmentSaver.java
+++ src/main/java/org/aesy/apartment/ApartmentSaver.java
@@ -3,6 +3,7 @@ package org.aesy.apartment;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class ApartmentSaver {
         this.apartmentRepository = apartmentRepository;
     }
 
+    @Order(1)
     @EventListener
     public void saveNewApartments(NewApartmentsEvent event) {
         List<Apartment> apartments = event.getApartments();
